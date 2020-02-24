d5b4de11057fc72f44e4d1882f448dd3f6c4a7eb
ayesha <bsef17m038@pucit.edu.pk>
2020-02-24 23:19:18 +0500

    ajax call in signin.php

--- api.php
+++ api.php
@@ -8,12 +8,14 @@ $conn = new mysqli($servername, $user, $pass, $dbname);
 if (!$conn) {
     die("connection failed:" . mysqli_connect_error());
 }
-echo "connected sucessfully";
 $id=$_SESSION["useri"];
 ?>
 <?php
 if(isset($_REQUEST["action"]) && !empty($_REQUEST["action"]))
 {
+    $action=$_REQUEST["action"];
+    if($action=="create")
+    {
     $foldername=trim($_REQUEST["newFolderName"]);
     $parentfolder1d=$_REQUEST["pfid"];
     $sql="select * from folder where userid='$id' and foldername='$foldername' and parentfolder='$parentfolder1d' ";
@@ -24,10 +26,36 @@ if(isset($_REQUEST["action"]) && !empty($_REQUEST["action"]))
         $sql="select * from folder where userid='$id' and foldername='$foldername' and parentfolder='$parentfolder1d' ";
         $result=mysqli_query($conn,$sql);
     }
-    $action=$_REQUEST["action"];
-    if($action="create")
-    {
         $sql="insert into folder(foldername,parentfolder,userid) values('$foldername','$parentfolder1d','$id')";
         $rec=$conn->query($sql);
+    }
+    if($action=="signin")
+    {
+        $error="";
+        $result;
+
+            $name=$_REQUEST["user"];
+            $pass=$_REQUEST["pass"];
+
+            $sql="select * from user where login='$name' and password='$pass'";
+
+            $result=mysqli_query($conn,$sql);
+            if($result)
+                $recordsFound=mysqli_num_rows($result);
+            else
+                $recordsFound = 0;
+            if($recordsFound)
+            {
+                $row = mysqli_fetch_assoc($result);
+                $_SESSION['useri']=$row["userid"];
+                $result=true;
+                echo json_encode($result);
+            }
+            else
+            {
+                $result=false;
+                echo json_encode($result);
+            }
+
     }
 }?>
--- signin.php
+++ signin.php
@@ -1,48 +1,42 @@
 <!DOCTYPE html>
-
-<?php
-require("conn.php");
-?>
-<?php
-$error="";
-$uname="";
-if(isset($_REQUEST["submit"]))
-{
-    $name=$_REQUEST["user"];
-    $pass=$_REQUEST["pass"];
-
-    $sql="select * from user where login='$name' and password='$pass'";
-
-    $result=mysqli_query($conn,$sql);
-    if($result)
-        $recordsFound=mysqli_num_rows($result);
-    else
-        $recordsFound = 0;
-echo "<h1>$name$pass$recordsFound</h1>";
-    if($recordsFound)
-    {
-        $row = mysqli_fetch_assoc($result);
-        $_SESSION['useri']=$row["userid"];
-        header('Location:home.php');
-    }
-    else
-    {
-        $error="invalid user name or password";
-    }
-    $conn->close();
-}?>
 <html>
+<head>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+</head>
 <body>
 
-<form action="signin.php">
+<!--<form action="signin.php">-->
     Name:
-    <input type="text" name="user" />
+    <input type="text" name="user" id="user" />
     <br/>
     Password:
-    <input type="password" name="pass" /><br>
-    <span style="color:red"><?php echo "$error"?></span>
-    <input type="submit" name="submit" value="submit"/>
+    <input type="password" name="pass" id="password"/><br>
+    <span style="color:red" id="invalidmsg"></span>
+    <input type="submit" name="submit" value="submit" id="submit"/>
     <a href="1.php">register</a>
-</form>
+<!--</form>-->
+<script>
+    $(document).ready(function(){
+        $("#submit").click(function()
+        {
+            var n=$("#user").val();
+            var i=$("#password").val();
+            var d={"action":"signin","user":n,"pass":i};
+            $.get("api.php",d,function(data)
+            {
+               if(data=="false")
+               {
+                   $("#invalidmsg").html("invalid username or password");
+               }
+               else
+               {
+                   window.location.href="home.php";
+               }
+            });
+        });
+    });
+</script>
 </body>
 </html>
