c91b521197a1f8e947893ca0f6dbbb22ea101a1e
ayesha <bsef17m038@pucit.edu.pk>
2020-02-25 19:49:02 +0500

    ajax request handle

--- api.php
+++ api.php
@@ -8,14 +8,16 @@ $conn = new mysqli($servername, $user, $pass, $dbname);
 if (!$conn) {
     die("connection failed:" . mysqli_connect_error());
 }
-$id=$_SESSION["useri"];
+
 ?>
 <?php
 if(isset($_REQUEST["action"]) && !empty($_REQUEST["action"]))
 {
     $action=$_REQUEST["action"];
+
     if($action=="create")
     {
+        $id=$_SESSION["useri"];
     $foldername=trim($_REQUEST["newFolderName"]);
     $parentfolder1d=$_REQUEST["pfid"];
     $sql="select * from folder where userid='$id' and foldername='$foldername' and parentfolder='$parentfolder1d' ";
@@ -33,7 +35,7 @@ if(isset($_REQUEST["action"]) && !empty($_REQUEST["action"]))
     {
         $error="";
         $result;
-
+        $id=$_SESSION["useri"];
             $name=$_REQUEST["user"];
             $pass=$_REQUEST["pass"];
 
@@ -58,4 +60,52 @@ if(isset($_REQUEST["action"]) && !empty($_REQUEST["action"]))
             }
 
     }
-}?>
+    if($action=="register")
+    {
+        $name=$_REQUEST["user"];
+        $login=$_REQUEST["login"];
+        $pass=$_REQUEST["pass"];
+        $result;
+//        if($pass!=$confPass)
+//        {
+//            $error="password donot match!";
+//        }
+        $sql="select * from user where login='$name'";
+        $result=mysqli_query($conn,$sql);
+        if($result)
+            $recordsFound=mysqli_num_rows($result);
+        else
+            $recordsFound = 0;
+        if($recordsFound==0)
+        {
+            $sql="insert into user(name,login,password) values('$name','$login','$pass')";
+            $conn->query($sql);
+            $sql="select * from user where login='$name' and password='$pass'";
+            $result=mysqli_query($conn,$sql);
+            $row = mysqli_fetch_assoc($result);
+            $_SESSION['useri']=$row["userid"];
+            $result=true;
+            echo json_encode($result);
+
+        }
+        else{
+            $result=false;
+            echo json_encode($result);
+        }
+
+//            if($conn->query($sql)===true)
+//            {
+////                $msg="registered sucessfully";
+////                header('Location:home.php');
+//                $result=true;
+//                echo json_encode($result);
+//            }
+//            else
+//            {
+//                $result=false;
+//                echo json_encode($result);
+//            }
+
+    }
+}
+$conn->close(); ?>
