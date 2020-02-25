19c33755d26c7020dbe13968da44f429d8146c28
ayesha <bsef17m038@pucit.edu.pk>
2020-02-25 19:49:27 +0500

    ajax request

--- signin.php
+++ signin.php
@@ -8,14 +8,17 @@
 <body>
 
 <!--<form action="signin.php">-->
-    Name:
-    <input type="text" name="user" id="user" />
-    <br/>
-    Password:
-    <input type="password" name="pass" id="password"/><br>
+<div class="form-group">
+    <label for="user"> Name:</label>
+    <input type="text" name="user" id="user" class="form-control w-25" placeholder="enter name"/><span style="color:red" id="userspan"></span>
+</div>
+<div class="form-group">
+    <label for="password">Password:</label>
+    <input type="password" name="pass" id="password" class="form-control w-25" placeholder="enter password"/><span style="color:red" id="passspan"></span>
     <span style="color:red" id="invalidmsg"></span>
-    <input type="submit" name="submit" value="submit" id="submit"/>
-    <a href="1.php">register</a>
+</div>
+    <input type="submit" class="btn btn-outline-dark" name="submit" value="submit" id="submit"/>
+    <a href="1.php" class="btn btn-outline-dark">register</a>
 <!--</form>-->
 <script>
     $(document).ready(function(){
@@ -23,10 +26,29 @@
         {
             var n=$("#user").val();
             var i=$("#password").val();
-            var d={"action":"signin","user":n,"pass":i};
-            $.get("api.php",d,function(data)
+            if(n==="" && i==="")
             {
-               if(data=="false")
+                $("#userspan").html("required");
+                $("#passspan").html("required");
+            }
+            else if(n==="")
+            {
+                $("#passspan").html("");
+                $("#userspan").html("required");
+            }
+            else if(i==="")
+            {
+                $("#userspan").html("");
+                $("#passspan").html("required");
+            }
+            else
+            {
+                $("#passspan").html("");
+                $("#userspan").html("");
+            var d={"action":"signin","user":n,"pass":i};
+            $.post("api.php",d,function(data)
+            {
+               if(data==="false")
                {
                    $("#invalidmsg").html("invalid username or password");
                }
@@ -35,6 +57,7 @@
                    window.location.href="home.php";
                }
             });
+            }
         });
     });
 </script>
