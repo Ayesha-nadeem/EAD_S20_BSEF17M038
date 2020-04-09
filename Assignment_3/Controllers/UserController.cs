e61f1609d4c886b79ccaee64218ada901436b093
ayesha <bsef17m038@pucit.edu.pk>
2020-04-09 13:22:40 +0500

    login created

---
+++ Assignment_3/Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebPrac.Security;
+
+namespace Assignment_3.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult ValidateUser(String login, String password)
+        {
+
+            Object data = null;
+
+            try
+            {
+                var url = "";
+                var flag = false;
+
+                var obj = PMS.BAL.UserBO.ValidateUser(login, password);
+                if (obj != null)
+                {
+                    flag = true;
+                    SessionManager.User = obj;
+                        url = Url.Content("~/Home/Index");
+                }
+
+                data = new
+                {
+                    valid = flag,
+                    urlToRedirect = url
+                };
+            }
+            catch (Exception)
+            {
+                data = new
+                {
+                    valid = false,
+                    urlToRedirect = ""
+                };
+            }
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
